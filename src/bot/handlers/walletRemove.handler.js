d958950e0c13fb68ad82492d3f001bee1ca09979
adrassad <adras.sad@gmail.com>
2026-01-09 15:04:03 +0300

    fix mistakes

--- src/bot/handlers/walletRemove.handler.js
+++ src/bot/handlers/walletRemove.handler.js
@@ -1,10 +1,8 @@
-// src/bot/handlers/walletRemove.handler.js
 import { BUTTONS } from '../constants/buttons.js';
 import { SCENES } from '../constants/scenes.js';
+import { RETURNS } from '../constants/returns.js';
 
 export function walletRemoveHandler(bot) {
-  console.log('REMOVE_WALLET scene =', SCENES.REMOVE_WALLET);
-
   bot.hears(BUTTONS.REMOVE_WALLET, async (ctx) => {
     ctx.session.returnTo = RETURNS.MAIN_MENU;
     await ctx.scene.enter(SCENES.REMOVE_WALLET);
