a3ca4bcec3a05c6579ac62d141d2d07f41952685
adrassad <adras.sad@gmail.com>
2026-02-03 11:12:48 +0300

    redis opt

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,10 @@
 // src/app/index.js
-import { bootstrapApp } from './bootstrap.js';
-import { startRuntime } from './runtime.js';
+import { bootstrapApp } from "./bootstrap.js";
+import { startRuntime } from "./runtime.js";
+import { connectRedis } from "../redis/redis.client.js";
 
 export async function startApplication() {
+  await connectRedis();
   await bootstrapApp();
   await startRuntime();
 }
