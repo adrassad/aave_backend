1ffded0f130de1dea082289d5e23b702d08524af
adrassad <adras.sad@gmail.com>
2026-03-09 08:22:04 +0300

    networks API, README

--- src/api/server.js
+++ src/api/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import healthRoute from "./routes/health.route.js";
 import assetsRoute from "./routes/assets.route.js";
 import pricesRoute from "./routes/prices.route.js";
+import networksRoute from "./routes/network.route.js";
 import apiLimiter from "./middlewares/rateLimit.middleware.js";
 import { ENV } from "../config/env.js";
 
@@ -16,14 +17,16 @@ export function startServer() {
   app.use("/health", apiLimiter);
   app.use("/assets", apiLimiter);
   app.use("/price", apiLimiter);
+  app.use("/networks", apiLimiter);
 
   // **подключаем роуты**
   app.use("/health", healthRoute);
   app.use("/assets", assetsRoute);
   app.use("/price", pricesRoute);
+  app.use("/networks", networksRoute);
 
-  app.listen(ENV.PORT, () => {
-    console.log(`🚀 Backend running on http://localhost:${ENV.PORT}`);
+  app.listen(ENV.PORT_API, () => {
+    console.log(`🚀 Backend running on http://localhost:${ENV.PORT_API}`);
   });
 
   return app;
